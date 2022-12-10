053340ddd2caedb1f9d44e364130ace8d6618fde
Ankur Gupta <82110808+ankurgupta032@users.noreply.github.com>
2022-12-10 16:11:42 +0530

    final

--- js/script.js
+++ js/script.js
@@ -1,85 +1,82 @@
 const cards = document.querySelectorAll('.memory-card');
 
-let hasFlippedCard = false;
-let lockBoard = false;
-let firstCard, secondCard;
+  let hasFlippedCard = false;
+  let lockBoard = false;
+  let firstCard , secondCard;
+  let counter = 12;
+    counterupdate();
 
-//Initial moves and win count
-let movesCount = 6;
 
-  const time = document.getElementById('time');
-  time.innerHTML=`<span>Try's Remaining:</span>${movesCount}`;
 
-  //For calculating moves
-const movesCounter = () => {
-    console.log(movesCount);
-    if(movesCount==1){
-        time.innerHTML=`<span>Try Again:</span>`;
-        resetBoard();
-      }
-    movesCount -= 1;
-    time.innerHTML = `<span>Try's Remaining:</span>${movesCount}`;
-  };
-
-function flipCard() {
-  if (lockBoard) return;
-  if (this === firstCard) return;
-
-  this.classList.add('flip');
-
-  if (!hasFlippedCard) {
+    function unflipCards() {
+        setTimeout(() => {
+          firstCard.classList.remove('flip');
+          secondCard.classList.remove('flip');
     
-    movesCounter();
-    hasFlippedCard = true;
-    firstCard = this;
+          resetBoard();
+        }, 1500);
+      }
+    
+      function resetBoard() {
+        [hasFlippedCard, lockBoard] = [false, false];
+        [firstCard, secondCard] = [null, null];
+      }
 
-    return;
+      
+  function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+    counterupdate();
+
+
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+      hasFlippedCard = true;
+      firstCard = this;
+      return;
+    }
+
+    secondCard = this;
+    lockBoard = true;
+
+    checkForMatch();
   }
 
-  secondCard = this;
-  checkForMatch();
-}
-
-function checkForMatch() {
-  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
-
-  isMatch ? disableCards() : unflipCards();
-}
-
-function disableCards() {
-  firstCard.removeEventListener('click', flipCard);
-  secondCard.removeEventListener('click', flipCard);
-  var wincount=0;
-  wincount+=1;
-    console.log(wincount);
-  if (wincount == 5) {
-     time.innerHTML = `<span>You Won</span>`;
+  function checkForMatch() {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    isMatch ? disableCards() : unflipCards();
   }
 
-  resetBoard();
-}
-
-function unflipCards() {
-  lockBoard = true;
-
-  setTimeout(() => {
-    firstCard.classList.remove('flip');
-    secondCard.classList.remove('flip');
+  function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
 
     resetBoard();
-  }, 1500);
-}
+  }
 
-function resetBoard() {
-  [hasFlippedCard, lockBoard] = [false, false];
-  [firstCard, secondCard] = [null, null];
-}
+  
 
-(function shuffle() {
-  cards.forEach(card => {
-    let randomPos = Math.floor(Math.random() * 12);
-    card.style.order = randomPos;
-  });
-})();
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+  function counterupdate(){
+    if(counter <= 0){
+        alert('Better Luck Next Time');
+        location.reload();
+        
+        document.getElementById('remainCo').innerHTML = `Try Remaining: ` + Math.ceil(counter/2);
+      }
+      else{
+        
+      document.getElementById('remainCo').innerHTML = `Try Remaining: ` + Math.ceil(counter/2);
+      counter--;
+      }
+  }
+
+ (function shuffle() {
+   cards.forEach(card => {
+     let ramdomPos = Math.floor(Math.random() * 12);
+     card.style.order = ramdomPos;
+   });
+ })();
+
+  cards.forEach(card => card.addEventListener('click', flipCard));
